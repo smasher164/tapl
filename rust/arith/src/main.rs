ba2752f95626b15a577f5325566e38470a6a18ed
smasher164 <aindurti@gmail.com>
2021-10-10 00:31:32 -0400

    parse args

--- rust/arith/src/main.rs
+++ rust/arith/src/main.rs
@@ -1,3 +1,29 @@
+use std::env;
+use std::fs::File;
+use std::process;
+
+fn usage() -> ! {
+    eprintln!("usage: arith ( -small-step | -big-step ) file\n");
+    eprintln!("arith is an implementation of the untyped calculus");
+    eprintln!("of booleans and numbers (TAPL chapter 3 & 4).");
+    process::exit(2);
+}
+
+fn small_step(file: File) {}
+
+fn big_step(file: File) {}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<_> = env::args().skip(1).collect();
+    let str_args: Vec<_> = args.iter().map(|s| s.as_str()).collect();
+    let (eval, filename): (fn(File), &&str) = match &str_args[..] {
+        ["-small-step", filename] => (small_step, filename),
+        ["-big-step", filename] => (big_step, filename),
+        _ => usage(),
+    };
+    let mut file = File::open(filename).unwrap_or_else(|err| {
+        eprintln!("{}", err);
+        usage();
+    });
+    eval(file);
 }
